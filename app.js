2ecd89284578d0e6e13ad560e874beaf7c03e012
aleks-andrs <0602.id@gmail.com>
2020-05-08 02:23:31 +0100

    Added backend

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./config/db');
+const passport = require('passport');
+
+//Connect database
+mongoose.connect(
+  config.db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+);
+
+//Display database connection confirmation
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database: '+config.db);
+});
+
+//Display possile connection error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+//Port number
+const port = 4000;
+
+//CORS
+app.use(cors());
+
+//Angular
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser
+app.use(bodyParser.json());
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+//Routing
+app.get('/', (req, res) =>{
+  res.send('Invalid request');
+});
+
+app.get('*',(req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+//Start server
+app.listen(port, () => {
+  console.log('Server started on port ' + port);
+});
---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  db: 'mongodb://alex:passcode@cluster0-shard-00-00-4yf85.mongodb.net:27017,cluster0-shard-00-01-4yf85.mongodb.net:27017,cluster0-shard-00-02-4yf85.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  secret: 'secret'
+}
